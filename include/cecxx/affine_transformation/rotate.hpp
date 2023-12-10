2e08e43c9a8c421370203dd7611f137031086c5d
ewarchul <ewarchul@gmail.com>
2023-12-10 22:24:33 +0100

    [WIP] Attempt to add universal rotator

--- include/cecxx/affine_transformation/rotate.hpp
+++ include/cecxx/affine_transformation/rotate.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
 #include "cecxx/types.hpp"
+#include <iostream>
+#include <mdspan/mdspan.hpp>
 #include <range/v3/numeric/inner_product.hpp>
+#include <range/v3/view/cartesian_product.hpp>
+#include <range/v3/view/iota.hpp>
 #include <range/v3/view/transform.hpp>
 
 namespace cecxx::affine {
@@ -10,8 +14,25 @@ using namespace cecxx::types;
 template <typename Matrix> struct rotate {
   rotate(Matrix rotate_matrix) : rotate_{std::move(rotate_matrix)} {}
   auto operator()(numeric_range auto &&xs) {
-    return rotate_ | ranges::views::transform([&](auto &&row) {
-             return ranges::inner_product(row, xs, 0);
+    auto span_mat = Kokkos::mdspan{
+        rotate_.data(), Kokkos::extents{rotate_.rows(), rotate_.cols()}};
+    for (int i{0}; i < rotate_.rows(); ++i) {
+      for (int j{0}; j < rotate_.cols(); ++j) {
+        std::cout << span_mat[i, j];
+      }
+      std::cout << std::endl;
+    }
+
+    auto rows_num =
+        ranges::views::iota(0, static_cast<int>(span_mat.extent(0)));
+    return rows_num | ranges::views::transform([span_mat, &xs](auto &&row_num) {
+             auto mat_row =
+                 Kokkos::submdspan(span_mat, row_num, Kokkos::full_extent);
+             auto mat_row_span =
+                 std::span{mat_row.data_handle(), mat_row.size()};
+             std::cout << "mat row span -> " << mat_row_span[0]
+                       << mat_row_span[1] << mat_row_span[2] << std::endl;
+             return ranges::inner_product(mat_row_span, xs, 0);
            });
   }
 
--- main.cpp
+++ main.cpp
@@ -1,27 +1,30 @@
 #include "cecxx/affine_transformation/rotate.hpp"
+#include <Eigen/Core>
 #include <cecxx/affine_transformation/AffineTransformation.hpp>
 #include <cecxx/unimodal/Unimodal.hpp>
 #include <iostream>
 #include <vector>
 
-
 auto main() -> int {
-  auto scaler_2 = cecxx::affine::scale{2};
-  auto rot_matrix = std::vector{
-    std::vector{1, 2, 3},
-    std::vector{4, 5, 6},
-    std::vector{7, 8, 9}
-  };
+  // auto scaler_2 = cecxx::affine::scale{2};
+  // auto rot_matrix = std::vector{
+  //   std::vector{1, 2, 3},
+  //   std::vector{4, 5, 6},
+  //   std::vector{7, 8, 9}
+  // };
+  Eigen::MatrixXd eigen_rot_matrix{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+  Eigen::MatrixXd eigen_rot_matrix_t = eigen_rot_matrix.transpose();
+  std::cout << eigen_rot_matrix << std::endl;
+  auto rotator = cecxx::affine::rotate{eigen_rot_matrix_t};
   //  int rot_matrix_c[2][3] = {{1, 2, 3}, {4, 5, 6}};
-  auto rotator = cecxx::affine::rotate{rot_matrix};
   const auto x = std::vector<double>{1, 2, 3};
   auto xr = rotator(x);
   std::cout << "rotated: " << xr << std::endl;
-  auto y = cecxx::unimodal::sphere(x, scaler_2);
-  int xx[] = {2, 2, 2};
-  auto yy = cecxx::unimodal::sphere(xx, scaler_2);
-  std::cout << y << std::endl;
-  std::cout << yy << std::endl;
+  // auto y = cecxx::unimodal::sphere(x, scaler_2);
+  // int xx[] = {2, 2, 2};
+  // auto yy = cecxx::unimodal::sphere(xx, scaler_2);
+  // std::cout << y << std::endl;
+  // std::cout << yy << std::endl;
 
   return 0;
 }
